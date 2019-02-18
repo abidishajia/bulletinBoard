bddda9341b5e5a0d49a411b5a65b6e06c0b67a15
abidishajia <abidishajia@gmail.com>
2019-02-17 22:05:05 -0800

    Updates

---
+++ precache-manifest.e34debe2c15faaaf84877a45d3ce42dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "883393396df879707537",
+    "url": "/bulletinBoard/static/js/runtime~main.88339339.js"
+  },
+  {
+    "revision": "3afd8b3d5b16ab926487",
+    "url": "/bulletinBoard/static/js/main.3afd8b3d.chunk.js"
+  },
+  {
+    "revision": "93d104cccb110c9014de",
+    "url": "/bulletinBoard/static/js/1.93d104cc.chunk.js"
+  },
+  {
+    "revision": "3afd8b3d5b16ab926487",
+    "url": "/bulletinBoard/static/css/main.df5636c7.chunk.css"
+  },
+  {
+    "revision": "6dd5af1b5327b21ae53c109ae699574d",
+    "url": "/bulletinBoard/index.html"
+  }
+];
